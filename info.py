c0686a89e6a8595fce9a70f0a5d16278d62920dc
wade231 <122469077+wade231@users.noreply.github.com>
2023-02-14 16:29:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/SL_Auto_Filter_Bot_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/SL_Auto_Filter_Bot_Updates')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/nxtbots')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Animebotinfo')
 
 # Bot settings
 AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
@@ -53,7 +53,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 
 # Other
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "✅ I Found: <code>{query}</code>\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating} / 10</a>\n☀️ Languages: {languages}\n📀 RunTime: {runtime} Minutes\n\n🗣 Requested by: {message.from_user.mention}\n©️ Powered by: <b>{message.chat.title}</b>")
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n\n©️ Powered by: <a href=https://t.me/Hansaka_Anuhas>Hansaka Anuhas</a>")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n\n©️ Powered by: <a href=https://t.me/ragnar_zxc>Ragnar Lothbrok</a>")
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 
 # Log
